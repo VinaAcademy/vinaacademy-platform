0c548b012102348e760c7dc8327e0431384ba9b8
Nguyen Huu Loc <locn562836@gmail.com>
2025-05-31 17:16:43 +0700

    feat(cache): implement caching for category retrieval and management with configurable TTL

---
+++ src/main/java/com/vinaacademy/platform/configuration/cache/CacheConstants.java
@@ -0,0 +1,8 @@
+package com.vinaacademy.platform.configuration.cache;
+
+public class CacheConstants {
+    public static final String CATEGORY = "categories";
+    public static final String CATEGORIES = "categories";
+    public static final String SHORT_TERM = "shortTerm";
+    public static final String LONG_TERM = "longTerm";
+}
